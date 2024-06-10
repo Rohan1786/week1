39da11b58df2bf8357e9e1f9d6462feec15dff47
Rohan1786 <rp9522203@gmail.com>
2024-06-10 16:57:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var sc=document.querySelector("#container");
+var heart=document.querySelector("i");
+
+sc.addEventListener("dbclick",()=>{
+    heart.style.transform='translate(-50%,-50%) scale(1)';
+    heart.style.opacity=0.8;
+
+})
